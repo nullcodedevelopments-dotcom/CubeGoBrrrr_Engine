479991957924dfcb8786418402222a4de33570cc
Loopy <syndexpy@gmail.com>
2026-05-01 03:19:59 +0530

    added some fail safes

--- src/Core/Utils/event_bus.py
+++ src/Core/Utils/event_bus.py
@@ -6,14 +6,20 @@ class Config:
     # checks for file type to get the correct path
     if isinstance(segments[0], (list,tuple)):
       segments = segments[0]
-    
-    with open(filename, "r") as file:
-      data = json.load(file)
+    try:
+      with open(filename, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    except:
+      return ["", {}]
     # loops through the segment to get the final json
     main = data
     path = segments
     for i in segments:
-      main = main[i]
+      try:
+        main = main[i]
+      except KeyError as e:
+        print(e)
+        return [path, {}]
     # path is the segmdnts list and main is the requested data
     return [path, main]
   
@@ -22,20 +28,27 @@ class Config:
     path = obj[0]
     data = obj[1]
     
-    with open(filename, "r") as file:
-      full_data = json.load(file)
+    try:
+      with open(filename, "r", encoding="utf-8") as file:
+        full_data = json.load(file)
+    except:
+      full_data = data
     
     # if whole json is chnaged then update it whole
-    if not path:
-      full_data = data
-    else:
-      current = full_data
-      for i in path[:-1]:
-        current = current[i]
-      current[path[-1]] = data
-      
-    with open(filename, "w") as file:
+    try:
+      if not path:
+        full_data = data
+      else:
+        current = full_data
+        for i in path[:-1]:
+          current = current[i]
+        current[path[-1]] = data
+    except KeyError as e:
+      print(e)
+
+    with open(filename, "w", encoding="utf-8") as file:
       json.dump(full_data, file, indent=4)
+    
 
 # Example Usage
 if __name__ == "__main__":
